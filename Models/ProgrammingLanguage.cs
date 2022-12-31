46dbbafa00f62b12ad78ff1bc75676286d65b5a5
Sidachenko Michael <michaellux@yandex.ua>
2023-01-01 00:32:14 +0300

    Small fix in label

--- Models/ProgrammingLanguage.cs
+++ Models/ProgrammingLanguage.cs
@@ -2,13 +2,15 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ArtsofteTestProject.Models;
 
+[Display(Name = "Programming language")]
 public partial class ProgrammingLanguage
 {
     public Guid Id { get; set; }
-
+    
     public string Name { get; set; }
 
     public virtual ICollection<EmployeePlace> EmployeePlace { get; } = new List<EmployeePlace>();
